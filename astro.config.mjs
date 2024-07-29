a8c4b84575ea4c83e0c5bf433798548e2bf65d1b
Alain <alainmh23@gmail.com>
2024-07-29 14:44:39 -0500

    *

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import robotsTxt from "astro-robots-txt";
 import icon from "astro-icon";
-import vercel from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt(), icon(), react()],
   site: "https://useplanify.com/",
-  trailingSlash: "never",
-  output: "server",
-  adapter: vercel(),
+  trailingSlash: "never"
 });
